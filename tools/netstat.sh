30e8778acbff373ae7ad0ffcf1a861d5a90b1573
berniey <berniey@dev.superbiiz.com>
2016-01-06 16:00:13 -0800

    add netstat count by port

---
+++ tools/netstat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+netstat -nat | awk '/^tcp/ {n=split($(NF-2),array," ");if(n<=2)++S[array[(1)]];else++S[array[(4)]];++s[$NF];++N} END {for(a in S){printf("%-20s %s\n", a, S[a]);++I}printf("%-20s %s\n","TOTAL_IP",I);for(a in s) printf("%-20s %s\n",a, s[a]);printf("%-20s %s\n","TOTAL_LINK",N);}' | sort -n -k2
